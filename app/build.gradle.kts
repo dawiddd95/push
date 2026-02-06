f8b7a4491c8ca843e72700d200b01c6e02947d7c
Dawid Łychoński <dawlyc1995@gmail.com>
2026-02-06 13:29:03 +0100

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,7 +52,8 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    
+    implementation("com.google.android.gms:play-services-location:21.1.0")
+
     // WebSocket client
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     
