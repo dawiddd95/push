093980426c8761b1615fd7c560691a7dcc74b688
Dawid Łychoński <dawlyc1995@gmail.com>
2026-02-06 13:31:25 +0100

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.soundalarm"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2.0"
     }
 
     buildTypes {
@@ -52,10 +52,12 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("com.google.android.gms:play-services-location:21.1.0")
-
+    
     // WebSocket client
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    
+    // Location
+    implementation("com.google.android.gms:play-services-location:21.1.0")
     
     // Coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
